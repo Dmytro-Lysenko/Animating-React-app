72951f71bf264b4e19da12946a04459810aaf08f
Dmytro Lysenko <crystaljoint@gmail.com>
2021-12-06 17:26:42 +0200

    add animation to modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,23 @@
-import React from 'react';
 
-import './Modal.css';
+  
+import React from "react";
 
-const modal = (props) => (
-    <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
+import "./Modal.css";
+
+const modal = props => {
+  const cssClasses = [
+    "Modal",
+    props.show ? "ModalOpen" : "ModalClosed"
+  ];
+
+  return (
+    <div className={cssClasses.join(' ')}>
+      <h1>A Modal</h1>
+      <button className="Button" onClick={props.closed}>
+        Dismiss
+      </button>
     </div>
-);
+  );
+};
 
 export default modal;
